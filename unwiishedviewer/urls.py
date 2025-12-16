a823d7a626c07ff7f15424f74a55bf41aca7f419
erobards <emr972@utulsa.edu>
2025-12-16 14:22:48 -0600

    updated urls to hopefully work remotely?

--- unwiishedviewer/urls.py
+++ unwiishedviewer/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path, re_path
 from .views import *
 urlpatterns = [
     re_path(r'stage/(?P<pk>[\d]+)', StageDetailView.as_view(), name="stageviewer"),
-    path('',StageListView.as_view(), name="stagelist"),
+    path('stages/',StageListView.as_view(), name="stagelist"),
 
 ]
